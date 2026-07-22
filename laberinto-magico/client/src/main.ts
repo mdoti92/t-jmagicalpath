f9e5e74972edf5be7df2ad31f9a48c029b87c4eb
Darkwiner <tomas_cancela18@hotmail.com>
2026-07-22 12:29:51 -0300

    Ventana responsive

--- laberinto-magico/client/src/main.ts
+++ laberinto-magico/client/src/main.ts
@@ -9,6 +9,10 @@ const config: Phaser.Types.Core.GameConfig & { createDOMContainer?: boolean } = 
   createDOMContainer: true,
   width: 960,
   height: 640,
+  scale: {
+    mode: Phaser.Scale.FIT, // Escala el juego para encajar en el contenedor
+    autoCenter: Phaser.Scale.CENTER_BOTH, // Lo centra horizontal y verticalmente
+  },
   scene: [LobbyScene, BoardScene, UIScene],
 };
 
